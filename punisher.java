ee5881e6faa6698b2780f309e64f799ce4e710b8
Manoj M <88279843+Manojmuthusamy@users.noreply.github.com>
2022-10-10 15:53:24 +0530

    Update punisher.java

--- punisher.java
+++ punisher.java
@@ -2,6 +2,6 @@ public class Punisher{
    public static void main(String[] args){
        System.out.println("One batch");
        System.out.println("Two Batch");
-       System.out.println("Penny and Dime");
+       System.out.println("Penny and Die");
    }
 }
